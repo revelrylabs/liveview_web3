1935dedef4c7abffad2d7430adb1824bb8e0fbae
Crystal Adkins <CrystalAdkins09@gmail.com>
2022-03-19 14:46:01 -0400

    Format config file

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,34 +1,34 @@
 module.exports = {
-  // Uncommenting the defaults below
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
-  networks: {
-   development: {
-     host: "127.0.0.1",
-     port: 7545,
-     network_id: "*"
-   }
-   // test: {
-   //   host: "127.0.0.1",
-   //   port: 7545,
-   //   network_id: "*"
-   // }
-  },
-  //
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
+    // Uncommenting the defaults below
+    // provides for an easier quick-start with Ganache.
+    // You can also follow this format for other networks;
+    // see <http://truffleframework.com/docs/advanced/configuration>
+    // for more details on how to specify configuration options!
+    //
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        }
+        // test: {
+        //   host: "127.0.0.1",
+        //   port: 7545,
+        //   network_id: "*"
+        // }
+    },
+    //
+    // Truffle DB is currently disabled by default; to enable it, change enabled:
+    // false to enabled: true. The default storage location can also be
+    // overridden by specifying the adapter settings, as shown in the commented code below.
+    //
+    // NOTE: It is not possible to migrate your contracts to truffle DB and you should
+    // make a backup of your artifacts to a safe location before enabling this feature.
+    //
+    // After you backed up your artifacts you can utilize db by running migrate as follows:
+    // $ truffle migrate --reset --compile-all
+    //
+    // db: {
     // enabled: false,
     // host: "127.0.0.1",
     // adapter: {
@@ -37,10 +37,10 @@ module.exports = {
     //     directory: ".db"
     //   }
     // }
-  // }
+    // }
     compilers: {
-    solc: {
-      version: "0.8.10",
-    }
-  },
+        solc: {
+            version: "^0.8.10",
+        }
+    },
 };
