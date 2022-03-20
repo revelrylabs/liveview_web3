108381b4ca1ac0013ba4dc06ccc1a8c8550fcff3
Crystal Adkins <CrystalAdkins09@gmail.com>
2022-03-20 16:23:24 -0400

    Update styling

--- lib/web3_x_liveview_web/live/page_live.ex
+++ lib/web3_x_liveview_web/live/page_live.ex
@@ -9,7 +9,82 @@ defmodule Web3XLiveviewWeb.PageLive do
   @impl true
   def render(assigns) do
     ~H"""
+    <.container>
+      <div class="flex justify-center py-4">
+        <.h2>Transactions</.h2>
+      </div>
+      <div class="flex place-content-between">
+        <div class="flex place-content-start">
+          <%= live_render(@socket, Web3XLiveviewWeb.MetamaskButtonLive,
+            id: "connect",
+            session: %{"id" => "metamask-login", "text" => "Log in with Metamask"}
+          ) %>
+        </div>
+        <div class="flex place-content-end">
+          <div class="p-2">
+            <.button size="lg" label="Mint NFT" class="ml-auto" to="/" />
+          </div>
+          <div class="p-2">
+            <.button size="lg" label="Coin Transaction" class="ml-auto" to="/"/>
+          </div>
+        </div>
+      </div>
 
+      <div class="pt-6">
+        <.table>
+          <.tr>
+            <.th>Public Address</.th>
+            <.th>Contract Name</.th>
+            <.th>Method Called</.th>
+            <.th>Email</.th>
+          </.tr>
+
+          <.tr>
+            <.td>
+              John Smith
+            </.td>
+            <.td>Engineer</.td>
+            <.td>john.smith@example.com</.td>
+          </.tr>
+
+          <.tr>
+            <.td>
+              Beth Springs
+            </.td>
+            <.td>Developer</.td>
+            <.td>beth.springs@example.com</.td>
+          </.tr>
+
+          <.tr>
+            <.td>
+              Peter Knowles
+            </.td>
+            <.td>Programmer</.td>
+            <.td>Peter.knowles@example.com</.td>
+          </.tr>
+
+          <.tr>
+            <.td>
+              Sarah Hill
+            </.td>
+            <.td>Marketer</.td>
+            <.td>sarah.hill@example.com</.td>
+          </.tr>
+        </.table>
+      </div>
+    </.container>
+    """
+  end
+
+  defp table_row(assigns) do
+    ~H"""
+    <.tr>
+        <.td>
+          John Smith
+        </.td>
+        <.td>Engineer</.td>
+        <.td>john.smith@example.com</.td>
+      </.tr>
     """
   end
 end
