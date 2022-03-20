6c3f4619a62568af65eade2d6295db21895ef36d
Crystal Adkins <CrystalAdkins09@gmail.com>
2022-03-20 01:04:18 -0400

    Remove unneeded contracts, add signature and token contracts

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,10 +1,7 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
 const VerifySignature = artifacts.require("VerifySignature");
+const CovidVaccineToken = artifacts.require("CovidVaccineToken");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
   deployer.deploy(VerifySignature);
+  deployer.deploy(CovidVaccineToken);
 };
